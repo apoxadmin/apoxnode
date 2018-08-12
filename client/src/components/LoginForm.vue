dbd7709ed205a6421e6ddbf8940fd0ef9a00d8b9
danielduyvo <danielduyvo@yahoo.com>
2018-08-11 21:54:54 -0700

    Added a login route

---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,19 @@
+<template>
+        <div class="login">
+          <form>
+              <label for="username">User Name</label>
+              <input type="text" name="username" required>
+              <br />
+              <label for="password">Password</label>
+              <input type="password" name="password" required>
+              <br />
+              <button type="submit">Login</button>
+          </form>
+        </div>
+</template>
+
+<script>
+export default {
+  name: "LoginForm"
+};
+</script>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+        <div class="login">
+          <h1>This is a login page</h1>
+          <LoginForm />
+        </div>
+      </template>
+      
+      <script>
+// @ is an alias to /src
+import LoginForm from "@/components/LoginForm.vue";
+
+export default {
+  name: "login",
+  components: {
+    LoginForm
+  }
+};
+</script>
